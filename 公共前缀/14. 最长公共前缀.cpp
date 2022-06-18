dae6fc656360323ec4962af0a38145463792c26f
minnakamiyuki <1669761932@qq.com>
2022-06-18 22:29:38 +0800

    修改了程序中的小错误

--- 公共前缀/14. 最长公共前缀.cpp
+++ 公共前缀/14. 最长公共前缀.cpp
@@ -7,14 +7,14 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string> strs) {
         const int n = strs.size(), len = strs[0].size();
-        if (strs.size() == 0)
+        if (n == 0)
             return "";
         for (int i = 0; i < len; i++) {
             char c = strs[0][i];
-            for (int j = 0; j < strs.size(); j++)
+            for (int j = 1; j < n; j++)
                 if (i == strs[j].size() || c != strs[j][i])
                     return strs[0].substr(0, i);  // 0开始的i个 0到i-1
         }
-        return strs[0].substr(0, len); //如果没有返回值，说明前n个字符都相同
+        return strs[0].substr(0, len);  // 如果没有返回值，说明前n个字符都相同
     }
 };
